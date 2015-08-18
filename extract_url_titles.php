e11777afce7b9f3115398cd3ae0d3473cf7e2209
kirai <hectorgarcia@gmail.com>
2015-08-18 18:44:08 +0900

    extracts titles from a list of urls

---
+++ extract_url_titles.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Description: extracts titles from a list of urls
+ * It follows redirects and works with Japanese (Shift-JIS or UTF-8 encoded)
+ * CC: Hector Garcia http://www.ageekinjapan.com
+ */
+
+mb_language('Japanese');
+
+$urls = explode(",", file_get_contents('textfile.txt'));
+
+function file_get_contents_curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+foreach($bits as $url) {
+
+    $html = file_get_contents_curl(trim($url));
+    preg_match('/<title>(.+)<\/title>/',$html,$matches);
+    $title = $matches[1];
+
+    echo $url . ';' . $title . "\n";
+}
